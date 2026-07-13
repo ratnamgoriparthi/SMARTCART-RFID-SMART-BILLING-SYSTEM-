101f61bd867b1a511dc2c69423052add036ad9b1
Goriparthi Lakshmi venkata ratnam <121328922+ratnamgoriparthi@users.noreply.github.com>
2026-07-13 20:13:54 +0530

    Delete Front end directory

--- Front end/Header files/delay.h
+++
@@ -1,4 +0,0 @@
-//function declarations for delay
-void delay_us(int dlyus); //in terms of microseconds
-void delay_ms(int dlyms);//in terms of milliseconds
-void delay_s(int dlys);//in terms of seconds
--- Front end/Header files/keypad.h
+++
@@ -1,7 +0,0 @@
-#include <lpc21xx.h>
-#define ROWS 16  //p1.16
-#define COLS 20  //p1.20
-
-void init_kpm(void);//Initialize keypad GPIO pins
-char keyscan(void);//Scan keypad and return pressed key
-int read_num(void);//Read multi-digit number from keypad 
