ed741f2faa4063bd5c2a164299aba11bae190b09
Goriparthi Lakshmi venkata ratnam <121328922+ratnamgoriparthi@users.noreply.github.com>
2026-07-13 20:09:26 +0530

    Add files via upload

---
+++ Front end/Header files/delay.h
@@ -0,0 +1,4 @@
+//function declarations for delay
+void delay_us(int dlyus); //in terms of microseconds
+void delay_ms(int dlyms);//in terms of milliseconds
+void delay_s(int dlys);//in terms of seconds
